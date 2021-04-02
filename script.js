aa3ec6fecc3c4c7ead382d130b3c3fbf10dcd978
Nikol Mrazova <nikolikrat@seznam.cz>
2021-04-02 15:04:37 +0200

    refactor: Function addTask was refactored

--- script.js
+++ script.js
@@ -2,35 +2,11 @@ const addTaskBtn = document.body.querySelector("#button-addon2");
 
 function addTask() {
     const inputValue = document.body.querySelector("#todo-input").value;
-    const newItem = document.createElement("div");
-    newItem.innerText = inputValue;
-    newItem.className = "flex-grow-1";
     const label = document.createElement("label");
     label.className = "list-group-item";
-    const div1 = document.createElement("div");
-    div1.className = "d-flex align-items-center";
-    const div2 = document.createElement("div");
-    div2.className = "me-auto";
-    const checkbox = document.createElement("input");
-    checkbox.className = "form-check-input me-2";
-    checkbox.type = "checkbox";
-    const div3 = document.createElement("div");
-    const editBtn = document.createElement("button");
-    editBtn.className = "btn btn-outline-primary btn-sm ms-2";
-    editBtn.innerText = "edit";
-    const div4 = document.createElement("div")
-    div4.className = "bd-highlight";
-    const closeBtn = document.createElement("button");
-    closeBtn.className = "btn-close btn-sm ms-2";
+    label.innerHTML =
+        '<div class="d-flex align-items-center"><div class="me-auto"><input class="form-check-input me-2" type="checkbox" value=""></div><div class="flex-grow-1">' + inputValue + '</div><div class=""><button class="btn btn-outline-primary btn-sm ms-2">edit</button></div><div class="bd-highlight"><button type="button" class="btn-close btn-sm ms-2"></button></div></div>';
     document.body.querySelector("#todo-tasks").appendChild(label);
-    label.appendChild(div1);
-    div1.appendChild(div2);
-    div2.appendChild(checkbox);
-    div1.appendChild(newItem);
-    div1.appendChild(div3);
-    div3.appendChild(editBtn);
-    div1.appendChild(div4);
-    div4.appendChild(closeBtn);
 }
 
 addTaskBtn.addEventListener("click", addTask);
