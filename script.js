7b410593b0a58dc03e77cda04b6bfcd02879c187
Nikol Mrazova <nikolikrat@seznam.cz>
2021-04-05 22:23:49 +0200

    refactor: Add background color and few fixes

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function addTask() {
                 <div class="flex-grow-1 task-name">
                     ${inputValue}
                 </div>
-                <div><button class="btn btn-outline-primary btn-sm ms-2" data-bs-toggle="modal" data-bs-target="#exampleModal">edit</button></div>
+                <div><button class="btn btn-outline-primary btn-sm ms-2" data-bs-toggle="modal" data-bs-target="#editTaskModal">edit</button></div>
                 <div class="bd-highlight"><button type="button" class="btn-close btn-sm ms-2"></button>
                 </div>
             </div>`
