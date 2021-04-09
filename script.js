2e610701dbc967fa0739cf962ea55b763c43101f
Nikol Mrazova <nikolikrat@seznam.cz>
2021-04-09 22:26:36 +0200

    refactor: Replace this with event.target, make variables instead of querySelectors, rename variables, move modal into html

--- script.js
+++ script.js
@@ -1,42 +1,17 @@
 //contants
 const KEYPRESS_ENTER = 13
+const taskList = document.body.querySelector("#todo-tasks");
+const addTaskBtn = document.body.querySelector("#buttonAddTask");
+const todoInput = document.body.querySelector("#todo-input");
 
 // state variables
 let lastEditedTaskButton = undefined;
 
-// modal storage
-const modalTask = document.createElement('div');
-modalTask.innerHTML =
-    `<div class="modal fade" id="editTaskModal" tabindex="-1" aria-labelledby="editTaskModal" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="editTaskModal">Edit a task</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <div class="mb-3">
-                        <label for="message-text" class="col-form-label">Task:</label>
-                        <textarea class="form-control" id="message-text" style="height: 150px"></textarea>
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary" data-bs-dismiss="modal"
-                        id="saveChangesButton">Save changes</button>
-                </div>
-            </div>
-        </div> 
-    </div>`
-document.body.querySelector('.container').appendChild(modalTask)
-
 //after clicking button "Add" new task is added
-const addTaskBtn = document.body.querySelector("#button-addon2");
-
 function addTask() {
-    const inputValue = document.body.querySelector("#todo-input").value;
+    const inputValue = todoInput.value;
     if (inputValue) {
-        const task = document.body.querySelector('li');
+        const task = document.body.querySelector('#exampleTask');
         const newTask = task.cloneNode(true);
         newTask.classList.remove("hidden")
         newTask.querySelector('.task-name').innerText = inputValue
@@ -44,8 +19,8 @@ function addTask() {
         buttonClose.addEventListener('click', deleteTask, false);
         const checkbox = newTask.querySelector('.form-check-input');
         checkbox.addEventListener('click', taskDone, false);
-        document.body.querySelector("#todo-tasks").appendChild(newTask);
-        document.body.querySelector("#todo-input").value = "";
+        taskList.appendChild(newTask);
+        todoInput.value = "";
     }
 }
 
@@ -56,12 +31,9 @@ for (let i = 0; i < deleteBtn.length; i++) {
     deleteBtn[i].addEventListener('click', deleteTask, false);
 }
 
-function deleteTask() {
-    this.closest('.list-group-item').remove();
+function deleteTask(event) {
+    event.target.closest('.list-group-item').remove();
 }
-
-//hitting enter creates a new task
-const todoInput = document.body.querySelector("#todo-input");
 
 addTaskBtn.addEventListener("click", addTask);
 todoInput.addEventListener("keypress", function (e) {
@@ -77,8 +49,8 @@ for (let i = 0; i < checkboxes.length; i++) {
     checkboxes[i].addEventListener('click', taskDone, false);
 }
 
-function taskDone() {
-    this.closest('.list-group-item').classList.toggle("checkedTask");
+function taskDone(event) {
+    event.target.closest('.list-group-item').classList.toggle("checkedTask");
 }
 
 //editing tasks
